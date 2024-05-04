4c1a3e93816c239ea5ad6d8ca674249532854094
Jan Lim <jan.borja.lim@gmail.com>
2024-05-04 12:41:21 -0700

    added booking seed data

---
+++ backend/db/seeders/20240504193606-demo-bookings.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const { SpotImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await SpotImage.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20"
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20"
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20"
+      },
+      {
+        spotId: 1,
+        userId: 2,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20"
+      },
+      {
+        spotId: 1,
+        userId: 3,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20"
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20"
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        startDate: "2021-11-19",
+        endDate: "2021-11-20"
+      },
+      {
+        spotId: 3,
+        userId: 2,
+        startDate: "2021-10-19",
+        endDate: "2021-10-20"
+      },
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
