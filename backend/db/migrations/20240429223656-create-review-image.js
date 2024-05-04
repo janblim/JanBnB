d159fdc88ea62b5573094778a1bbc326c13eb9e6
Jan Lim <jan.borja.lim@gmail.com>
2024-05-04 15:33:10 -0700

    ready for deply

--- backend/db/migrations/20240429223656-create-review-image.js
+++ backend/db/migrations/20240429223656-create-review-image.js
@@ -1,5 +1,8 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('ReviewImages', {
@@ -26,6 +29,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    options.tableName = "ReviewImages";
+    return queryInterface.dropTable(options);
   }
 };
