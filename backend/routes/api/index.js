75d6a42a4180f19e23419cb897214e00130d60e7
Jan Lim <jan.borja.lim@gmail.com>
2024-05-04 08:35:12 -0700

    added Create a Review based on Spot id

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
+const reviewsRouter = require('./reviews.js');
 
 
 // router.post('/test', function(req, res) {
@@ -47,6 +48,7 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
+router.use('/reviews', reviewsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,81 +1,26 @@
 const express = require('express');
 // const bcrypt = require('bcryptjs');
 
-const { requireAuth } = require('../../utils/auth');
-const { Spot, Review, SpotImage } = require('../../db/models');
+// const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Review } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// Get all Spots
-
-router.get(
-        '/',
-        async (req, res) => {
-            const spots = await Spot.findAll()
-            return res.status(200).json({"Spots": spots})
-        }
-)
-
-// Get all Spots from current user
+// Get all Reviews from current user
 router.get(
     '/current',
     async (req, res) => {
         const { user } = req;
-        const spots = await Spot.findAll(
-            {where: {'ownerId': user.id}}
+        const reviews = await Review.findAll(
+            {where: {'userId': user.id}}
         )
-        return res.status(200).json({"Spots": spots})
+        return res.status(200).json({"Reviews": reviews})
     }
 )
 
-// Get details of a Spot from an id
-
-router.get(
-    '/:spotId',
-    async (req, res) => {
-        const id = req.params.spotId;
-
-        const spot = await Spot.findByPk(id)
-
-          if(!spot){
-            return res.status(404).json({
-              "message": "Spot couldn't be found"
-            });
-          } else {
-
-            const numReviews = await Review.count({where:{spotId:id}});
-
-            const sumRatings = await Review.sum('stars', {where:{spotId:id}});
-            const avgStarRating = sumRatings/numReviews;
-
-            const spotImages = await SpotImage.findAll({
-                where: {spotId:id}
-            })
-
-
-            const spotData = {
-                id: spot.id,
-                ownerId: spot.ownerId,
-                address: spot.address,
-                city: spot.city,
-                state: spot.state,
-                country: spot.country,
-                lat: spot.lat,
-                lng: spot.lng,
-                name: spot.name,
-                description: spot.description,
-                price: spot.price,
-                numReviews: numReviews,
-                avgStarRating: avgStarRating,
-                SpotImages: spotImages
-            }
-            return res.status(200).json(spotData)
-          }
-    }
-)
 
 //Spot validator
 
@@ -186,7 +131,6 @@ router.post(
 router.put(
     '/:spotId',
     validateSpot,
-    requireAuth,
     async (req, res) => {
 
         const spotId = req.params.spotId;
@@ -234,7 +178,6 @@ router.put(
 
 router.delete(
     '/:spotId',
-    requireAuth,
     async (req, res) => {
 
         const spotId = req.params.spotId;
@@ -256,78 +199,5 @@ router.delete(
     }
     }
 );
-
-// Get Reviews by a Spot's id
-
-router.get(
-    '/:spotId/reviews',
-    async (req, res) => {
-        const spotId = req.params.spotId;
-
-        const spot = await Spot.findByPk(spotId)
-
-        if(!spot){
-            return res.status(404).json({
-                message: "Spot couldn't be found"
-            })
-        } else {
-
-        const reviews = await Review.findAll(
-            {where: {'spotId': spotId}}
-        )
-        return res.status(200).json({"Reviews": reviews})
-        }
-    }
-);
-
-//validate Review
-
-const validateReview = [
-    check('review')
-      .exists({ checkFalsy: true })
-      .withMessage("Review text is required"),
-    check('stars')
-        .isInt({min: 1, max: 5})
-        .withMessage("Stars must be an integer from 1 to 5"),
-    handleValidationErrors
-  ];
-
-// Create a Review based on a Spot's id
-router.post(
-    '/:spotId/reviews',
-    validateReview,
-    requireAuth,
-    async (req, res) => {
-
-    const { review, stars } = req.body;
-    const spotId = parseInt(req.params.spotId);
-    const userId = req.user.id;
-    const spotCheck = await Spot.findByPk(spotId);
-    const userCheck = await Review.findAll({
-        where: { userId: userId}
-    })
-
-    if(!spotCheck){
-        return res.status(404).json({
-            message: "Spot couldn't be found"
-        })
-    } else if (userCheck){
-        return res.status(500).json({
-            message: "User already has a review for this spot"
-        })
-    } else {
-
-    const newReview = await Review.create({
-        userId: userId,
-        spotId: spotId,
-        review,
-        stars });
-
-    return res.status(201).json(newReview)
-
-    }
-}
-);
-
 
 module.exports = router;
