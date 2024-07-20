7af2a5a5e016314272f473d33ed314f9271089ec
Jan Lim <jan.borja.lim@gmail.com>
2024-07-20 15:08:27 -0700

    created cards in  manage spots page

--- frontend/src/components/ManageSpots/ManageSpots.jsx
+++ frontend/src/components/ManageSpots/ManageSpots.jsx
@@ -1,12 +1,14 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllUserSpotsThunk } from '../../store/spot';
+import Card from '../Card/Card';
+import './ManageSpots.css'
 
 
 const ManageSpots = () => {
 
     const dispatch = useDispatch();
-    const userSpots = useSelector((state) => state.spotState.allUserSpots)
+    const userSpots = useSelector((state) => state.spotState.allUserSpots.allSpots)
 
     useEffect( () => {
         const getUserSpots = async() => dispatch(getAllUserSpotsThunk());
@@ -14,18 +16,31 @@ const ManageSpots = () => {
         }, []);
 
   return (
-    <div>
-
-        {userSpots.allSpots.map(spot => (
-            <div key={`${spot.id}-${spot.address}`}>
-                <span style={{cursor: 'pointer'}}>
-
-                        {spot.address}
-
-                </span>
-            </div>
-        ))}
-    </div>
+    <>
+        <div>
+            <h1>Manage Your Spots</h1>
+            <button>Create a New Spot</button>
+        </div>
+        <div id='card-container'>
+            {userSpots.map(spot => (
+                <div key={`${spot.id}-${spot.address}`}>
+                    <span>
+                        <Card
+                        preview={spot.previewImage}
+                        city={spot.city}
+                        state={spot.state}
+                        rating={spot.avgRating}
+                        price={spot.price}
+                        />
+                    </span>
+                    <span className='button-box'>
+                        <button className='update'>Update</button>
+                        <button className='delete'>Delete</button>
+                    </span>
+                </div>
+            ))}
+        </div>
+    </>
   );
 }
 
